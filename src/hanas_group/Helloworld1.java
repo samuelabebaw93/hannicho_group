f5ed35d54f6fabba71fe3a57078af25008bc331d
samuelabebaw93 <samuelabebaw93@gmail.com>
2020-10-21 20:56:52 -0400

    first commit

---
+++ src/hanas_group/Helloworld1.java
@@ -0,0 +1,14 @@
+package hanas_group;
+
+public class Helloworld1 {
+ public static void main(String[] args) {
+	
+	 int myvariable;
+	 myvariable = 6;
+	 int hana = 7;
+	  int sum = myvariable + hana;
+	 System.out.println(myvariable + hana);
+	 System.out.println(sum);
+}
+ 
+}
